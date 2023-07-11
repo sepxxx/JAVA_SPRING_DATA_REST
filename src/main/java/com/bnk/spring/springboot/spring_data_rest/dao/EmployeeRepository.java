c63d4638c55765fdd92356221d002f1d7c992d2c
Jeraldine Davon <jeraldin@pr-k5.kzn.21-school.ru>
2023-07-11 11:06:32 +0300

    deleted controllers and services

---
+++ src/main/java/com/bnk/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.bnk.spring.springboot.spring_data_rest.dao;
+
+
+import com.bnk.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
